d92dda9c3b514d70f5170130c17a33960f77c795
jecan <alumno@viralgamingacademy.com>
2019-09-17 13:04:51 +0200

    Prototipo 6 Done

--- Assets/_PlantsVsZombies/Scripts/Zombie.cs
+++ Assets/_PlantsVsZombies/Scripts/Zombie.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
+    //velocidad del zombie
+    public float speed;
+    // el zombie se esta muriendo
+    public bool isDead;
+    //el zombie esta comiendo
+    public bool isEating;
 
     void Start()
     {
+        //inicializamos variables
+        isDead = false;
+        isEating = false;
     }
 
     void Update()
-    {
+    {// si el zombi no esta muriendose ni esta comiendo
+        if (isEating == false && isDead == false)
+        {
+            //moverse
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+
     }
 
     public void Die()
     {
+        isDead = true;
+        Destroy(this.gameObject, 0.1f);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Estoy chocando con ..." + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Plants"))
+        {
+            //isEating = true;
+            Destroy(collision.gameObject);
+        }
+
+
     }
 }
