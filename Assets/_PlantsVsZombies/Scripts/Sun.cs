7a8b711aab8f1a60d687faf6f23b567bf6bb0715
jecan <alumno@viralgamingacademy.com>
2019-09-17 12:01:39 +0200

    proto 5 done

--- Assets/_PlantsVsZombies/Scripts/Sun.cs
+++ Assets/_PlantsVsZombies/Scripts/Sun.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class Sun : MonoBehaviour
 {
+
+    public Animator myAnimator;
+
+    public float currentDestroyTime;
+    public float destroyTime;
+
+    public bool isTaken;
+
    
     private void Start()
     {
+
+        isTaken = false;
+        currentDestroyTime = 0;
     }
 
     private void Update()
     {
+
+
     }
+
+    private void OnMouseDown()
+    {
+        if (isTaken == false)
+        {
+            isTaken = true;
+            myAnimator.SetTrigger("Taken");
+            
+        }
+    }
+
+    public void destroySun()
+    {
+        Destroy(this.gameObject, 0f);
+    }
+
 }
--- Assets/_PlantsVsZombies/Scripts/Sunflower.cs
+++ Assets/_PlantsVsZombies/Scripts/Sunflower.cs
@@ -4,19 +4,56 @@ using UnityEngine;
 
 public class Sunflower : MonoBehaviour
 {
-  void Start()
+
+    //el tiempo que hace falta para que se recargue el disparo
+    public float reloadTime;
+    // tiempo restante 
+    public float currentCDTime;
+    // variable que nos indica si se esta ejecutando la animacion de generar
+    public bool isGenerating;
+    //PRefab de sun para generarlo con instanciate
+    public GameObject prefabSun;
+    // referencia del Animador
+    public Animator myAnimator;
+
+    void Start()
   {
+        currentCDTime = 0;
+        isGenerating = false;
   }
 
   void Update()
-  {   
+  {
+        //si no estamos disparando , seguimos sumando tiempo
+        if (isGenerating == false)
+        {
+            //sumamos el tiempo desde el ultimo frame
+            currentCDTime += Time.deltaTime;
+            //si el tiempo que ha pasado es mayor que el de recarga significa que
+            // ya hemos recargado
+            if(currentCDTime >= reloadTime)
+            {
+
+                // empezamos a generar el sol
+                isGenerating = true;
+                StartGenerateAnimation();
+            }
+        }
   }
 
   public void StartGenerateAnimation()
   {
+        myAnimator.SetTrigger("Generate");
+
   }
 
   public void GenerateSun()
   {
-  }
+
+        GameObject createdPea = Instantiate(prefabSun);
+
+        currentCDTime = 0;
+        isGenerating = false;
+
+    }
 }
